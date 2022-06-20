465fad23983f7303114ebff0c5ab40b24c9aab58
qkrwogns9703 <jaehunb350@gmail.com>
2022-06-20 23:47:08 +0900

    refactor: 드래그앤 드랍 fxts 함수 적용

--- src/Components/TodoList/TodoList.tsx
+++ src/Components/TodoList/TodoList.tsx
@@ -5,6 +5,8 @@ import TodoItem from '../../Components/TodoItem';
 import { Todo } from 'types/todoTypes';
 import { DragEvent } from 'react';
 
+import { pipe, slice, append, toArray, concat } from '@fxts/core';
+
 function TodoList() {
   let sourceElement: HTMLElement | undefined = undefined;
 
@@ -23,18 +25,18 @@ function TodoList() {
 
     const target = event.target as HTMLElement;
     if (sourceElement !== target) {
-      const list = myTodos.filter((_, i) => i.toString() !== sourceElement?.id);
+      const list = myTodos.filter((_, index) => String(index) !== sourceElement?.id);
 
-      const removed = myTodos.filter((_, i) => i.toString() === sourceElement?.id)[0];
-      let insertAt = Number(target.id);
+      const removed = myTodos.filter((_, index) => String(index) === sourceElement?.id)[0];
+      const insertAt = Number(target.id);
       let tempList;
 
       if (insertAt >= list.length) {
-        tempList = list.slice(0).concat(removed);
+        tempList = pipe(list, slice(0), append(removed), toArray);
         setMyTodos(tempList);
       } else if (insertAt < list.length) {
-        tempList = list.slice(0, insertAt).concat(removed);
-        const newList = tempList.concat(list.slice(insertAt));
+        tempList = pipe(list, slice(0, insertAt), append(removed), toArray);
+        const newList = pipe(slice(insertAt, list), concat(tempList), toArray);
         setMyTodos(newList);
       }
     }
